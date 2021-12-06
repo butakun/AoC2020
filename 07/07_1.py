1d7cb51a31f674cf0dc22e7ff776bf0889833785
Hiromasa Kato <hka@trackman.com>
2021-12-06 18:05:45 +0100

    day 7

---
+++ 07/07_1.py
@@ -0,0 +1,63 @@
+from pprint import pprint
+
+
+def main(filename):
+    with open(filename) as f:
+        bags = {}
+        for line in f:
+            line = line.strip()
+            line = line.replace("contains", "contain")
+            line = line.replace("bags", "bag")
+
+            outer, inners = line.split("contain")
+            tokens = outer.strip().split()
+            assert(tokens[-1] == "bag")
+            outer = " ".join(tokens[0:-1])
+
+            inners = inners.strip(".").split(",")
+            inners = [i.strip() for i in inners]
+            contains = {}
+            for inner in inners:
+                tokens = inner.split()
+                if tokens[0] == "no":
+                    num = 0
+                    assert tokens[1] == "other" and tokens[-1] == "bag"
+                else:
+                    num = int(tokens[0])
+                    assert tokens[-1] == "bag"
+                    color = " ".join(tokens[1:-1])
+                    assert color not in contains
+                    contains[color] = num
+            assert outer not in bags
+            bags[outer] = contains
+
+    pprint(bags)
+
+    graph = {}
+
+    for outer, contains in bags.items():
+        for color, num in contains.items():
+            if color in graph:
+                graph[color].add(outer)
+            else:
+                graph[color] = set([outer])
+
+    pprint(graph)
+
+    discovered = set()
+    def dfs(color):
+        discovered.add(color)
+        if color not in graph:
+            return
+        for outer in graph[color]:
+            if outer not in color:
+                dfs(outer)
+
+    dfs("shiny gold")
+    discovered.remove("shiny gold")
+    print(discovered)
+    print(len(discovered))
+
+
+if __name__ == "__main__":
+    main("input_debug_2.txt")
---
+++ 07/07_2.py
@@ -0,0 +1,49 @@
+from pprint import pprint
+
+
+def main(filename):
+    with open(filename) as f:
+        bags = {}
+        for line in f:
+            line = line.strip()
+            line = line.replace("contains", "contain")
+            line = line.replace("bags", "bag")
+
+            outer, inners = line.split("contain")
+            tokens = outer.strip().split()
+            assert(tokens[-1] == "bag")
+            outer = " ".join(tokens[0:-1])
+
+            inners = inners.strip(".").split(",")
+            inners = [i.strip() for i in inners]
+            contains = {}
+            for inner in inners:
+                tokens = inner.split()
+                if tokens[0] == "no":
+                    num = 0
+                    assert tokens[1] == "other" and tokens[-1] == "bag"
+                else:
+                    num = int(tokens[0])
+                    assert tokens[-1] == "bag"
+                    color = " ".join(tokens[1:-1])
+                    assert color not in contains
+                    contains[color] = num
+            assert outer not in bags
+            bags[outer] = contains
+
+    pprint(bags)
+
+    def dfs(color):
+        print(f"** {color} contains")
+        num_contains = 0
+        for inner, num in bags[color].items():
+            print(inner, num)
+            num_contains += num * dfs(inner)
+        return num_contains + 1
+
+    num_bags = dfs("shiny gold")
+    print(num_bags - 1)
+
+
+if __name__ == "__main__":
+    main("input.txt")
